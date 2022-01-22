18c30f9193cd9bdddd5b5ea935f1a5e737dfd8bb
Talwind1 <talwind1@gmail.com>
2022-01-22 23:57:07 +0200

    url change

--- client/api/Api.js
+++ client/api/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let myUrl = "http://localhost:8080/users"; //development
 
 if (process.env.NODE_ENV === "production") {
-  myUrl = "users";
+  myUrl = "";
 }
 export default axios.create({
   baseURL: myUrl,
